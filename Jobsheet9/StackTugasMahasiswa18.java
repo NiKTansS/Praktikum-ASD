c640620c8d3fba711a44a1d35e2d921adf79ea7e
nikytania <you@example.com>
2025-05-07 15:38:10 +0700

    Praktikum ASD

---
+++ Jobsheet9/StackTugasMahasiswa18.java
@@ -0,0 +1,83 @@
+package  Jobsheet9;
+
+public class StackTugasMahasiswa18 {
+  Mahasiswa18[] stack;
+  int top;
+  int size;
+
+  public StackTugasMahasiswa18(int size) {
+    this.size = size;
+    stack = new Mahasiswa18[size];
+    top = -1;
+  }
+  public boolean isFull() {
+    if (top == size -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public boolean isEmpty() {
+    if (top == -1) { 
+      return true;
+    } else {
+      return false;
+    }
+  }
+  public void push(Mahasiswa18 mhs) {
+    if (!isFull()) {
+      top++;
+      stack[top] = mhs;
+    } else {
+      System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+    }
+  }
+  public Mahasiswa18 pop() {
+    if (!isEmpty()) {
+      Mahasiswa18 m = stack[top];
+      top--;
+      return m;
+    } else {
+      System.out.println("Stack kosong! Tidak ada tugas untuk dinilai");
+      return null;
+    }
+  }
+  public Mahasiswa18 peek() {
+    if (!isEmpty()) {
+      return stack[top];
+    } else {
+      System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+      return null;
+    }
+  }
+  public void print() {
+    for (int i = top; i >= 0; i--) { //yang pertama mengumpulkan ada di bawah
+        System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+    }
+    System.out.println("");
+  }
+  public Mahasiswa18 bottom() {
+    if (!isEmpty()) {
+      return stack[0];
+    } else {
+      System.out.println("Stack kosong! Tidak ada tugas terbawah");
+      return null;
+    }
+  }
+  public int jumlahTugas() {
+    return top + 1;
+  }
+  public String konversiDesimalKeBiner(int nilai) {
+    StackKonversi18 stack = new StackKonversi18();
+    while (nilai != 0) {
+      int sisa = nilai % 2;
+      stack.push(sisa);
+      nilai = nilai / 2;
+    }
+    String biner = new String();
+    while (!stack.isEmpty()) {
+      biner += stack.pop();
+    }
+    return biner;
+  }
+}
