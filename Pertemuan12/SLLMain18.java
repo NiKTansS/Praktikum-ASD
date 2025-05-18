267f18c400b882505d0121b8b979c9947a5b3eb9
nikytania <you@example.com>
2025-05-18 22:19:33 +0700

    Linked List

--- Pertemuan12/SLLMain18.java
+++ Pertemuan12/SLLMain18.java
@@ -1,26 +1,31 @@
 package Pertemuan12;
+import java.util.Scanner;
 
 public class SLLMain18 {
   public static void main(String[] args) {
+    Scanner sc18 = new Scanner(System.in);
     SingleLinkedList18 sll = new SingleLinkedList18();
 
+    System.out.print("Masukkan jumlah Mahasiswa: ");
+    int jumlah = sc18.nextInt();
+    sc18.nextLine();
 
-    sll.print();
+    for(int i = 0; i < jumlah; i++){
+      System.out.println("Data Mahasiswa ke-" + (i + 1));
+      System.out.print("Nama    :");
+      String nama = sc18.nextLine();
+      System.out.print("NIM     :");
+      String nim = sc18.nextLine();
+      System.out.print("Kelas   :");
+      String kelas =sc18.nextLine();
+      System.out.print("IPK     :");
+      double ipk = sc18.nextDouble();
+      sc18.nextLine();
 
-    Mahasiswa18 mhs1 = new Mahasiswa18("22212202", "Cintia", "3C", 3.5);
-    Mahasiswa18 mhs2 = new Mahasiswa18("23212201", "Bimon", "2B", 3.8);
-    Mahasiswa18 mhs3 = new Mahasiswa18("24212200", "Alvaro", "1A", 4.0);
-    Mahasiswa18 mhs4 = new Mahasiswa18("21212203", "Dirga", "4D", 3.6);
 
-    //addFirst Dirga
-    sll.addFirst(mhs4);
-    sll.print();
-
-    sll.insertAfter("Dirga", mhs3);//insertAfter "Dirga" > Alvaro
-    sll.print();
-    
-    sll.insertAfter("Dirga", mhs1); //Cintia setelah Dirga
-    sll.insertAfter("Cintia", mhs2); //bimon setelah cintia
+      Mahasiswa18 mhs = new Mahasiswa18(nim, nama, kelas, ipk);
+      sll.addLast(mhs);
+    }
 
     sll.print();
   }
