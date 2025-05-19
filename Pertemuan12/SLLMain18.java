3c19da9b3e0a4788f04a3266c5ec9770ecd0156b
nikytania <you@example.com>
2025-05-19 21:49:21 +0700

    Linked List

--- Pertemuan12/SLLMain18.java
+++ Pertemuan12/SLLMain18.java
@@ -1,32 +1,36 @@
 package Pertemuan12;
-import java.util.Scanner;
 
 public class SLLMain18 {
   public static void main(String[] args) {
-    Scanner sc18 = new Scanner(System.in);
     SingleLinkedList18 sll = new SingleLinkedList18();
 
-    System.out.print("Masukkan jumlah Mahasiswa: ");
-    int jumlah = sc18.nextInt();
-    sc18.nextLine();
-
-    for(int i = 0; i < jumlah; i++){
-      System.out.println("Data Mahasiswa ke-" + (i + 1));
-      System.out.print("Nama    :");
-      String nama = sc18.nextLine();
-      System.out.print("NIM     :");
-      String nim = sc18.nextLine();
-      System.out.print("Kelas   :");
-      String kelas =sc18.nextLine();
-      System.out.print("IPK     :");
-      double ipk = sc18.nextDouble();
-      sc18.nextLine();
+    Mahasiswa18 mhs1 = new Mahasiswa18("22212202", "Cintia", "3C", 3.5);
+    Mahasiswa18 mhs2 = new Mahasiswa18("23212201", "Bimon", "2B", 3.8);
+    Mahasiswa18 mhs3 = new Mahasiswa18("24212200", "Alvaro", "1A", 4.0);
+    Mahasiswa18 mhs4 = new Mahasiswa18("21212203", "Dirga", "4D", 3.6);
 
 
-      Mahasiswa18 mhs = new Mahasiswa18(nim, nama, kelas, ipk);
-      sll.addLast(mhs);
-    }
+    // Tambah data dulu biar bisa diakses
+    sll.addFirst(mhs4);
+    sll.addLast(mhs1);
+    sll.addLast(mhs2);
+    sll.addLast(mhs3);
 
-    sll.print();
+     // Akses data index ke-1
+    System.out.println("data index 1 : ");
+    sll.getData(1); // Cintia
+
+    // Cari index berdasarkan nama
+    System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("Bimon"));
+    System.out.println();
+
+    // Hapus first dan last
+    sll.removeFirst(); // hapus Dirga
+    sll.removeLast();  // hapus Alvaro
+    sll.print();       // tersisa: Cintia, Bimon
+
+    // Hapus index ke-0 (Cintia)
+    sll.removeAt(0);
+    sll.print();       // tersisa: Bimon
   }
 }
