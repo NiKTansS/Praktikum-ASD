0f47b188cc3fd524afd41ab01b0186ede82dde0e
nikytania <you@example.com>
2025-05-21 14:19:18 +0700

    double linked list

---
+++ Pertemuan13DLL/Node01.java
@@ -0,0 +1,13 @@
+package Pertemuan13DLL;
+
+public class Node01 {
+  Mahasiswa01 data;
+  Node01 prev;
+  Node01 next;
+
+  public Node01(Mahasiswa01 data) {
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
